64ebd8f297398cbc2d4bd453063a746fa10ec627
armoken <alex.armoken@gmail.com>
2024-08-26 20:29:15 +0200

    Use Nix Flakes for application packaging

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='dmenu-systemd',
+      version='1.0',
+      # Modules to import from other scripts:
+      packages=find_packages(),
+      # Executables
+      scripts=["dmenu-systemd.py"],
+     )
